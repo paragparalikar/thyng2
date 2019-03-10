37a877c378fa19da86b141358273d4c7ac261f9e
Parag Paralikar <parag.p@inautix.co.in>
2019-03-10 12:15:20 +0530

    na

--- parent/device-registry/src/main/resources/public/pages/template/list.js
+++ parent/device-registry/src/main/resources/public/pages/template/list.js
@@ -7,16 +7,14 @@ $("#templates")
 							{
 								orderable : false,
 								mRender : function(data, type, row) {
-									
 									return '<div class="btn-group" role="group">'+
-										'<button type="button" class="btn btn-warning btn-xs" onclick="showViewTemplateModal(event, '+row.id+');">'+
+										'<button type="button" class="btn btn-warning btn-xs" onclick="showViewTemplateModal(this, event, '+row.id+');">'+
 											'<span class="fa fa-edit"></span> Edit'+
 										'</button>'+
 										'<button type="button" class="btn btn-danger btn-xs">'+
 											'<span class="fa fa-trash"></span> Delete'+
 										'</button>'+
 									'</div>';
-									
 								}
 							} ],
 					columnDefs: [{
@@ -29,8 +27,9 @@ $("#templates")
 					}
 				});
 
-var showViewTemplateModal = function(event, id) {
+var showViewTemplateModal = function(source, event, id) {
 	event.preventDefault();
+	source.blur();
 	$(".modal").load("pages/template/view.html", function() {
 		$(".modal").modal();
 		viewTemplateDetails(id);
--- parent/device-registry/src/main/resources/public/pages/template/view.js
+++ parent/device-registry/src/main/resources/public/pages/template/view.js
@@ -4,6 +4,9 @@ var viewTemplateDetails = function(id){
 		$("#templateDescription").val(template.description);
 		$("#inactivityPeriod").val(template.inactivityPeriod);
 		$("#templateTags").val(template.tags);
+		$("#templateProperties").val($.map(template.properties, function(value,key){
+			return key + " : " + value;
+		}).join("\n"));
 		$("#metrics-tab-content-title").text(template.name);
 		$("#metrics-table").DataTable({
 			searching: false,
@@ -17,8 +20,28 @@ var viewTemplateDetails = function(id){
 				{data: "unit"},
 				{data: "dataType"},
 				{data: "description"},
-				{data: "dataType"}
+				{
+					mRender : function(data, type, row) {
+						return '<div class="btn-group" role="group">'+
+							'<button type="button" class="btn btn-warning btn-xs" onclick="showMatricEditorModal(this, event, '+row.id+');">'+
+								'<span class="fa fa-edit"></span> Edit'+
+							'</button>'+
+							'<button type="button" class="btn btn-danger btn-xs">'+
+								'<span class="fa fa-trash"></span> Delete'+
+							'</button>'+
+						'</div>';
+					}
+				}
 			]
 		});
 	});
 }
+
+var showMatricEditorModal = function(source, event, id){
+	event.preventDefault();
+	source.blur();
+}
+
+$("#cancelButton").click(function(){
+	$.modal.close();
+});
