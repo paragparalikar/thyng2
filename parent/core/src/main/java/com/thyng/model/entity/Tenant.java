880c8194ecf239c8ae7613ccfe64c62f3e04f986
Parag Paralikar <parag.p@inautix.co.in>
2019-03-25 00:33:36 +0530

    na

--- parent/core/src/main/java/com/thyng/model/entity/Tenant.java
+++ parent/core/src/main/java/com/thyng/model/entity/Tenant.java
@@ -1,12 +1,8 @@
 package com.thyng.model.entity;
 
-import java.util.Set;
-
-import javax.persistence.CascadeType;
 import javax.persistence.Entity;
 import javax.persistence.GeneratedValue;
 import javax.persistence.Id;
-import javax.persistence.OneToMany;
 
 import lombok.AllArgsConstructor;
 import lombok.Builder;
@@ -29,11 +25,5 @@ public class Tenant extends AuditableEntity{
 	private String name;
 	
 	private String description;
-	
-	@OneToMany(cascade=CascadeType.ALL, orphanRemoval=true, mappedBy="tenant")
-	private Set<User> users;
-	
-	@OneToMany(cascade=CascadeType.ALL, orphanRemoval=true, mappedBy="tenant")
-	private Set<Thing> things;
 	
 }
